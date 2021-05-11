130fc78451b16c29518c63b45a562a4b2a67e6f1
haixei <aleksandraosuch002@gmail.com>
2021-05-11 16:52:31 +0200

    New correlation plot + lines in EDA

--- code/eda.py
+++ code/eda.py
@@ -15,9 +15,27 @@ corr_fig = pltx.imshow(corr, color_continuous_scale='Purpor')
 corr_fig.update_layout(title='Correlation between features')
 # >> corr_fig.show()
 
+def getDiscrete(age):
+    print(age)
+    if age < 29:
+        return 18
+    elif age > 29 and age <= 39:
+        return 30
+    elif age > 39 and age <= 49:
+        return 40
+    elif age > 49 and age <= 69:
+        return 50
+    else: return 60
+
+data['AgeMin'] = data.apply(lambda row: getDiscrete(row.Age), axis=1)
+print(data['AgeMin'])
+
 # Singular features against the target
-corr_lines_fig = pltx.parallel_categories(data.head(3000), color="NumOfProducts", color_continuous_scale='RdPu')
-# >> corr_lines_fig.show()
+corr_lines_fig = pltx.parallel_categories(data.head(3000), color="AgeMin", color_continuous_scale='RdPu')
+corr_lines_fig.show()
+
+# Drop the AgeMin column, it won't be useful later
+data = data.drop(['AgeMin'], axis=1)
 
 exited_fig = pltx.violin(data, y="Balance", x="Exited", color="Exited", box=True, points="all", color_discrete_sequence=['#ff4db5', '#b5abff'])
 # >> exited_fig.show()
